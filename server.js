babf214f918524bd5b3f1800a513f7c204ae7a32
Raghav <raghavendra.sao@mfine.co>
2020-07-08 23:39:01 +0530

    port for heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 app.use(express.static('public'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
